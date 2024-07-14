9dc44aa355489bb89b8fac254ff1b13b93837162
Zachary J McCann <zackjmccan@gmail.com>
2024-07-14 19:51:40 -0400

    change name of func

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def hello():
     return "Hello I am a small app!"
 
 @app.route("/pagetwo")
-def hello_2():
+def page_two():
     return "Hello from pagetwo of this small app!"
 
 
@@ -19,5 +19,5 @@ def server_error(e):
     logging.exception("An error occurred during a request.")
     return "An internal error occurred.", 500
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+	# app.run(host='0.0.0.0', port=5000)
