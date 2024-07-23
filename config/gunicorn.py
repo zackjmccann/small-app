d3904cd2a7125a7fdce86b9279a3974d26db198d
Zachary J McCann <zackjmccan@gmail.com>
2024-07-23 00:03:25 -0400

    gunicorn access log format

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -4,11 +4,10 @@ import multiprocessing
 import os
 from distutils.util import strtobool
 
-# TODO: Instead of binding to any interface on the network, bind to the external IP of container(?)
 bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 
 accesslog = "-"
-access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
+access_log_format = "%(t)s [%(h)s] | %(r)s (%(s)s) in %(D)sµs"  # noqa: E501
 
 workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
 threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
